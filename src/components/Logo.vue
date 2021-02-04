75845ab4ab603769e19a860fd44dd611f108fa00
Ygor Azambuja <ygorazambuja@gmail.com>
2021-02-04 09:52:27 -0400

    logo dinamica conforme o tema

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,30 @@
 <template>
   <v-layout align-center justify-center class="pa-5 mb-5">
-    <v-img max-width="370px" max-height="190px" src="../assets/logo-ufms.png" />
+    <v-img
+      max-width="370px"
+      max-height="190px"
+      :src="
+        isDark
+          ? require('../assets/ufms_logo_negativo_assinatura_vertical_rgb.png')
+          : require('../assets/ufms_logo_positivo_assinatura_vertical_rgb.png')
+      "
+      contain
+    />
   </v-layout>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    isDark: false,
+  }),
+
+  watch: {
+    "$vuetify.theme.dark"(val) {
+      this.isDark = !val;
+    },
+  },
+};
+</script>
 
 <style scoped></style>
