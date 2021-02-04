4c2022beae82dd1f57e6020dc9abc484062e8814
Ygor Azambuja <ygorazambuja@gmail.com>
2021-02-03 23:22:49 -0400

    alerta, e troca de temas

--- src/modules/login/pages/Login.vue
+++ src/modules/login/pages/Login.vue
@@ -1,9 +1,20 @@
 <template>
   <v-main>
     <v-container fluid>
+      <switch-theme></switch-theme>
       <logo />
       <v-layout align-center justify-center>
         <v-flex xs12 sm8 md6>
+          <v-alert
+            :value="alert"
+            color="pink"
+            dark
+            border="top"
+            icon="mdi-home"
+            transition="scale-transition"
+          >
+            {{ error }}
+          </v-alert>
           <v-card class="login-title elevation-12 pa-5 flex-column" shaped>
             <v-card-title class="pa-5 mx-auto">
               <h1 class="pt-5 pb-5">E-Prontuario</h1>
@@ -53,18 +64,22 @@
 <script>
 import Logo from "@/components/Logo.vue";
 import { mapActions } from "vuex";
+import SwitchTheme from "../../../components/SwitchTheme.vue";
 export default {
-  components: { Logo },
+  components: { Logo, SwitchTheme },
   data: () => ({
     show1: false,
     passaporte: "",
     senha: "",
+    alert: false,
+    error: "",
     loading: false,
     rules: {
       required: value => !!value || "Não pode ser vazio.",
       min: v => v.length >= 8 || "Minimo de 8 Caracteres"
     }
   }),
+
   methods: {
     ...mapActions("login", ["ActionSetGlobalUser"]),
     async doLogin() {
@@ -78,7 +93,15 @@ export default {
           this.loading = false;
           this.$router.push("/home");
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+          this.alert = true;
+          this.error = err;
+          this.loading = false;
+          setTimeout(() => {
+            this.alert = false;
+            this.error = "";
+          }, 10000);
+        });
     }
   }
 };
